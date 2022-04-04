c476d09868d061827497cb3487f8e2cf7b031e5d
Dylan <dylanchoi1@gmail.com>
2022-04-03 18:52:23 -0700

    Began training process
    Not sure if it's actually working though...

--- ai.py
+++ ai.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 import numpy as np
 import datetime
 import math
+from tqdm import tqdm
 
 from game import UTTT
 
@@ -48,7 +49,7 @@ class Node(object):
             num_boards, all_moves = game.move_list()
             x = torch.unsqueeze(torch.from_numpy(game.board_features()), 0).double()
             v, policy = neural_net(x)
-            policy = torch.reshape(policy, (9, 9))  # NOTE: Might cause some issues. If problems arise, double check this behavior
+            policy = torch.reshape(torch.exp(policy), (9, 9))  # NOTE: Might cause some issues. If problems arise, double check this behavior
             # total = torch.sum(torch.gather(policy, 0, torch.tensor(all_moves)))
 
             total = 0
@@ -85,7 +86,7 @@ class MCTS(object):
     def __init__(self, cpuct):
         self.cpuct = cpuct
     
-    def search(self, n_sims, neural_net, game_state):
+    def search(self, n_sims, neural_net, game_state, training):
         root = Node(0, None, game_state.turn)
         for _ in range(n_sims):
             current_node = root
@@ -117,8 +118,12 @@ class MCTS(object):
         policy = np.zeros((9, 9))
         for child in root.children:
             policy[child.move[0], child.move[1]] = child.visits
+        policy /= policy.sum()
 
-        chosen_node = random.choices(root.children, weights=[node.visits for node in root.children])[0]
+        if training:
+            chosen_node = random.choices(root.children, weights=[node.visits for node in root.children])[0]
+        else:
+            chosen_node = max(root.children, key=lambda node: node.visits)
         return [*chosen_node.move], policy
 
 
@@ -139,23 +144,62 @@ class NeuralNet(nn.Module):
         x = torch.flatten(x, 1)  # Flatten after the first dim wihle preserving the batch number
 
         x = F.relu(self.dense1(x))
-        policy = F.softmax(self.dense2(x), 1)
+        policy = F.log_softmax(self.dense2(x), 1)
         value = torch.tanh(self.dense3(x))
         return value, policy
 
 
-def gen_episodes(game, neural_net, n_eps=100):
-    tree = MCTS(4)
+class Player(object):
+    def __init__(self, n_sims=250, path=None):
+        self.brain = NeuralNet()
+        if path is not None:
+            self.brain.load_state_dict(torch.load(path))
+        self.brain.double()
+        self.n_sims = n_sims
+        self.optimizer = optim.Adam(self.brain.parameters())
+        self.v_loss = nn.MSELoss()
+        self.p_loss = lambda pred, real: -torch.sum(pred * real) / real.shape[0]
+    
+    def get_move(self, board, training=True):
+        tree = MCTS(4)
+        [num_board, move], policy = tree.search(self.n_sims, self.brain, board, training)
+        return num_board, move, policy
+    
+    def train(self, data: MCTSHistory, batch_size, epochs):
+        self.optimizer.zero_grad()
+        for i in range(epochs):
+            for batch in data.get_batch(batch_size):
+                X = torch.from_numpy(np.array([b[0] for b in batch]).reshape(-1, 3, 9, 9)).double()
+                pred_v, pred_p = self.brain(X)
+                real_p = torch.from_numpy(np.array([b[1] for b in batch]).reshape(-1, 81)).double()
+                real_v = torch.tensor([b[2] for b in batch]).double()
+                vloss, ploss = self.v_loss(pred_v, real_v.view(-1, 1)), self.p_loss(pred_p, real_p)
+                print(vloss, ploss)
+                loss = vloss + ploss
+                loss.backward()
+                self.optimizer.step()
+
+    def save_brain(self, path):
+        torch.save(self.brain.state_dict(), path)
+
+    def load_brain(self, path):
+        self.brain.load_state_dict(torch.load(path))
+    
+    def copy(self):
+        return deepcopy(self)
+
+
+def gen_episodes(player, game, n_eps=25):
     data_tracker = MCTSHistory()
     
     for _ in range(n_eps):
         prev_info = []
         game_copy = game.copy()
         while game_copy.result() == ' ':
-            [num_board, move], policy = tree.search(N_SIMS, neural_net, game_copy)
+            num_board, move, policy = player.get_move(game_copy)
             prev_info.append([game_copy.board_features(), policy])
             game_copy.move(num_board, move)
-            game_copy.print_board()
+            print(num_board, move)
 
         for v, info in enumerate(prev_info[::-1]):
             data_tracker.add_position(info[0], info[1], 1 if v % 2 == 0 else -1)
@@ -164,23 +208,54 @@ def gen_episodes(game, neural_net, n_eps=100):
     return data_tracker
 
 
-N_SIMS = 250
-brain = NeuralNet()
-brain.double()
-board = UTTT()
-optimizer = optim.SGD(brain.parameters(), lr=1e-3, momentum=0.9)
-v_loss, p_loss = nn.MSELoss(), nn.CrossEntropyLoss()
+# Pit 2 players against each other, return the player that wins over the cutoff, None otherwise
+def compete(player1, player2, cutoff=0.55, matches=10):
+    num_to_win = matches * cutoff
+    player1_wins = 0
+    player2_wins = 0
+    for _ in range(matches):
+        p1 = player1
+        p2 = player2
+        game = UTTT()
+        if random.choice([True, False]):  # 50 / 50 chance that p2 plays first
+            p1 = player2
+            p2 = player1
+        
+        while game.result() == ' ':
+            num_board, move, p = p1.get_move(game, training=False)
+            game.move(num_board, move)
+            if game.result() != ' ':
+                break
+            num_board, move, p = p2.get_move(game, training=False)
+            game.move(num_board, move)
+        
+        if game.result() == 'X':
+            if p1 == player1:
+                player1_wins += 1
+            else:
+                player2_wins += 1
+        elif game.result() == 'O':
+            if p1 == player1:
+                player2_wins += 1
+            else:
+                player1_wins += 1
+    if player1_wins >= num_to_win:
+        return player1
+    elif player2_wins >= num_to_win:
+        return player2
+    return None
 
-for _ in range(10):
-    optimizer.zero_grad()
-    data = gen_episodes(board, brain, 5)
-    for batch in data.get_batch():
-        X = torch.from_numpy(np.array([b[0] for b in batch]).reshape(-1, 3, 9, 9)).double()
-        pred_v, pred_p = brain(X)
-        real_p = torch.from_numpy(np.array([b[1] for b in batch]).reshape(-1, 81)).double()
-        real_v = torch.tensor([b[2] for b in batch]).double()
-        loss = v_loss(pred_v, real_v.view(-1, 1)) + p_loss(pred_p, real_p)
-        print(loss)
-        loss.backward()
-        optimizer.step()
-    torch.save(brain.state_dict(), r'C:\Users\dylan\Desktop\Code\Python\Ultimate-Tic-Tac-Toe\Models\{}.pt'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+
+
+prev_best = Player(n_sims=75)  # , path=r'C:\Users\dylan\Desktop\Code\Python\Ultimate-Tic-Tac-Toe\Models\20220403015231.pt'
+current_player = prev_best.copy() # Player(n_sims=100)
+board = UTTT()
+
+while True:
+    data = gen_episodes(current_player, board, 25)
+    current_player.train(data, batch_size=16, epochs=3)
+    result = compete(prev_best, current_player, cutoff=0.6, matches=10)
+    if result == current_player:
+        print('New Winner!')
+        current_player.save_brain(path=r'C:\Users\dylan\Desktop\Code\Python\Ultimate-Tic-Tac-Toe\Models\{}.pt'.format(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+        prev_best = current_player.copy()
